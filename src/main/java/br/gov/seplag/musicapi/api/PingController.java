3b9c9322e8e96732f64dd198ffbd6538f1f99652
Arão Farias <arao.farias@sefaz.ma.gov.br>
2026-01-23 16:15:53 -0300

    Iniciando projeto

---
+++ src/main/java/br/gov/seplag/musicapi/api/PingController.java
@@ -0,0 +1,16 @@
+package br.gov.seplag.musicapi.api;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class PingController {
+	@GetMapping("/ping")
+	public Map<String, String> ping() {
+		return Map.of("status", "ok");
+	}
+}
+
---
+++ src/test/java/br/gov/seplag/musicapi/MusicApiApplicationTests.java
@@ -0,0 +1,12 @@
+package br.gov.seplag.musicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
+
